04767c21f7ae89ffc73263ed15349d72c51c5911
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2019-11-22 13:44:35 +0100

    Fixed typo

--- 01/03/main.go
+++ 01/03/main.go
@@ -8,6 +8,9 @@ func main() {
 	stringVal := "Hello, world!"
 	intVal := 192
 	boolVal := true
+
+	//fmt.Println(stringVal + intVal + boolVal)
+
 	fmt.Println(stringVal, intVal, boolVal)
-	fmt.Printf("%s %d %b\n", stringVal, intVal, boolVal)
+	fmt.Printf("%s %d %t\n", stringVal, intVal, boolVal)
 }
